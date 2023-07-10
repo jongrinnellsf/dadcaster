3d0e56b7b364adc0c9c0444623a379ff8d9e83b3
Jon Grinnell <jongrinnell@Jons-MacBook-Pro.local>
2023-07-09 20:35:54 -0700

    full send

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Dadcaster.org',
-  tagline: 'In dad we trust',
+  tagline: 'Dadcaster is a sufficiently decentralized network of dads. Join us 👇',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
